6de4dff060404e2b7bb83bbd1f9f911133f1c9b7
banti27 <kumarbanti.kumar1@gmail.com>
2023-08-19 11:12:25 +0530

    Added Float Layout Component

---
+++ src/app/float-layout/float-layout.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FloatLayoutComponent } from './float-layout.component';
+
+describe('FloatLayoutComponent', () => {
+  let component: FloatLayoutComponent;
+  let fixture: ComponentFixture<FloatLayoutComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FloatLayoutComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FloatLayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/float-layout/float-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-float-layout',
+  templateUrl: './float-layout.component.html',
+  styleUrls: ['./float-layout.component.css']
+})
+export class FloatLayoutComponent {
+
+}
